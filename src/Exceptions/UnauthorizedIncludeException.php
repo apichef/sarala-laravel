69f1a20f3c2956238cf0cee1fdf060fe43088fc9
Milroy Fraser <milroy@outlook.com>
2019-01-05 16:28:18 +0530

    sanitizer includes test

--- src/Exceptions/UnauthorizedIncludeException.php
+++ src/Exceptions/UnauthorizedIncludeException.php
@@ -11,72 +11,18 @@ class UnauthorizedIncludeException extends ApiException
         parent::__construct($message, 403);
     }
 
-    /**
-     * Get unique identifier for this particular occurrence
-     * of the problem.
-     */
-    public function id(): ?string
-    {
-        return null;
-    }
-
-    /**
-     * Get the HTTP status code applicable to this problem.
-     */
     public function status(): int
     {
         return $this->getCode();
     }
 
-    /**
-     * Get application-specific error code.
-     */
-    public function code(): ?string
-    {
-        return null;
-    }
-
-    /**
-     * Get short, human-readable summary of the problem.
-     */
     public function title(): string
     {
         return 'Unacceptable include';
     }
 
-    /**
-     * Get human-readable explanation specific to this
-     * occurrence of the problem.
-     */
     public function detail(): ?string
     {
         return $this->getMessage();
-    }
-
-    /**
-     * Get the URI that yield further details about this
-     * particular occurrence of the problem.
-     */
-    public function href(): ?string
-    {
-        return null;
-    }
-
-    /**
-     * Get associated resources, which can be dereferenced
-     * from the request document.
-     */
-    public function links(): array
-    {
-        return [];
-    }
-
-    /**
-     * Get relative path to the relevant attribute within
-     * the associated resource(s).
-     */
-    public function path(): ?string
-    {
-        return null;
     }
 }
---
+++ tests/SanitizerIncludesTest.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Sarala;
+
+class SanitizerIncludesTest extends TestCase
+{
+    public function test_it_sanitizer_include()
+    {
+        $this->json('get', route('posts.index') . '?include=crap')
+            ->assertStatus(403)
+            ->assertJson([
+                'error' => [
+                    'status' => '403',
+                    'title' => 'Unacceptable include',
+                    'detail' => 'crap is missing in allowed includes. Allowed: tags,comments.author,author',
+                ]
+            ]);
+    }
+}
