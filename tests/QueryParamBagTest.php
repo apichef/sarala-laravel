c490cd98b327373a9bac9dc1de7ea8d1699dae29
PrinsFrank <f.prins@temper.works>
2022-08-04 12:50:51 +0200

    Add test case for falsey values not being converted to an empty array

--- tests/QueryParamBagTest.php
+++ tests/QueryParamBagTest.php
@@ -52,4 +52,15 @@ class QueryParamBagTest extends TestCase
         $this->assertFalse($filterParam->isEmpty('before'));
         $this->assertEquals($filterParam->get('before'), '21-02-2019');
     }
+
+    public function test_falsey_values_should_be_available(): void
+    {
+        $url = '/url?filter[comments]=&filter[amount]=0&filter[verified]=false';
+        $request = Request::create($url);
+        $filterParam = new QueryParamBag($request, 'filter');
+
+        $this->assertSame('', $filterParam->get('comments'));
+        $this->assertSame('0', $filterParam->get('amount'));
+        $this->assertSame('false', $filterParam->get('verified'));
+    }
 }
